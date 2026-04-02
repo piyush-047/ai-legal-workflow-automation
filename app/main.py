1c5d631e2030b4987ac668d06a26798115b7f33a
Piyush kumar <piyush8981ssm@gmail.com>
2026-04-02 16:43:03 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
+from rag_pipeline import get_response
 
+def main():
+    print("⚖️ AI Legal Assistant Started (type 'exit' to quit)\n")
+
+    while True:
+        query = input("Ask legal question: ")
+
+        if query.lower() == "exit":
+            break
+
+        response = get_response(query)
+        print("Answer:", response, "\n")
+
+if __name__ == "__main__":
+    main()
